a73192c99183c7e72a8779e1606b297314084f42
0warning0error <zhao_yuanhang@qq.com>
2025-09-28 23:37:19 +0800

    adapted to 4.19

--- kernel/kernel_compat.h
+++ kernel/kernel_compat.h
@@ -29,6 +29,7 @@ static inline __maybe_unused size_t list_count_nodes(const struct list_head *hea
     if (!head)
         return 0;
 
+extern int ksu_access_ok(const void *addr, unsigned long size);
     list_for_each(pos, head)
         count++;
 
